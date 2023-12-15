6b1e20d1857969d3a40606b0758bbab33a06eddd
nutsa.matchavariani <nutsa.matchavariani@energo-pro.ge>
2023-12-15 13:13:14 +0400

    Module #4.1. Final Task

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,38 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (year < 0) {
+            System.out.println("invalid date");
+            return;
+        }
 
+        if (month < 1 || month > 12) {
+            System.out.println("invalid date");
+            return;
+        }
+
+        int daysInMonth;
+
+        switch (month) {
+            case 1, 3, 5, 7, 8, 10, 12:
+                daysInMonth = 31;
+                break;
+            case 4, 6, 9, 11:
+                daysInMonth = 30;
+                break;
+            case 2:
+                daysInMonth = isLeapYear(year) ? 29 : 28;
+                break;
+            default:
+                System.out.println("invalid date");
+                return;
+        }
+
+        System.out.println( daysInMonth);
     }
+
+    private boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
 }
